818b1a36866529561ca5113c03e78c539362671d
murano <murano1980@gmail.com>
2018-11-29 00:33:45 +0300

    remove POLLOUT event

--- chapter2/run.c
+++ chapter2/run.c
@@ -5,18 +5,17 @@
 #include <string.h>
 #include "run.h"
 
-#define TIMEOUT 5
 #define BUFFSIZE 64
 
 int run() {
-    struct pollfd fds[2];
+    struct pollfd fds[1];
 
     char buff[BUFFSIZE];
     int fd;
     int ret;
     ssize_t wr;
     ssize_t rd;
-    int flag = 0;
+
     size_t current_len = 0;
 
     fd = open("../chapter2/data", O_APPEND | O_RDWR | O_CREAT, 0644);
@@ -25,21 +24,17 @@ int run() {
         perror("open");
     }
 
-
     fds[0].fd = STDIN_FILENO;
     fds[0].events = POLLIN;
 
-    fds[1].fd = fd;
-    fds[1].events = POLLOUT;
-
     for (;;) {
-        ret = poll(fds, 2, TIMEOUT * 10000);
+        ret = poll(fds, 1, -1);
         if (ret == -1) {
             perror("poll");
             return 1;
         }
 
-        if (fds[0].revents & POLLIN && flag == 0) {
+        if (fds[0].revents & POLLIN) {
             memset(buff,'\0',BUFFSIZE);
             rd = read(STDIN_FILENO, &buff, BUFFSIZE);
 
@@ -50,18 +45,12 @@ int run() {
                 return 1;
             }
 
-            flag = 1;
-        }
-
-        if (fds[1].revents & POLLOUT && flag == 1) {
             wr = write(fd, &buff, current_len);
 
             if (wr == -1) {
                 perror("write");
                 return 1;
             }
-
-            flag = 0;
         }
     }
 }
